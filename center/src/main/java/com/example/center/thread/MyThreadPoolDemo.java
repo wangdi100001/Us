20ce79071da0a8bd25a1ea243cd9313be13f545a
wangdi <wangdiclass@163.com>
2025-12-07 21:29:05 +0800

    添加手写线程

--- center/src/main/java/com/example/center/thread/MyThreadPoolDemo.java
+++ center/src/main/java/com/example/center/thread/MyThreadPoolDemo.java
@@ -1,11 +1,44 @@
 package com.example.center.thread;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class MyThreadPoolDemo {
 
     public static void main(String[] args) {
+
+//        useExecutorServicedDemo();
+        customizeExecutorServicePool();
+
+    }
+
+    /**
+     * 自定义线程池
+     */
+    private static void customizeExecutorServicePool() {
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println(runtime.availableProcessors());
+
+//        ExecutorService executorService = Executors.newFixedThreadPool(5);
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        ExecutorService executorService = new ThreadPoolExecutor(2, 5,
+                1L, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(3),Executors.defaultThreadFactory(),new ThreadPoolExecutor.CallerRunsPolicy());
+
+        /*new ThreadPoolExecutor.AbortPolicy();
+        new ThreadPoolExecutor.DiscardOldestPolicy();
+        new ThreadPoolExecutor.CallerRunsPolicy();
+        new ThreadPoolExecutor.DiscardPolicy();*/
+
+        for (int i = 1; i <= 10; i++) {
+            int finalI = i;
+            executorService.execute(()-> System.out.println(Thread.currentThread().getName()+",正在执行:"+ finalI));
+        }
+    }
+
+    /**
+     * 使用线程池
+     */
+    private static void useExecutorServicedDemo() {
         Runtime runtime = Runtime.getRuntime();
         System.out.println(runtime.availableProcessors());
 
@@ -17,8 +50,6 @@ public class MyThreadPoolDemo {
             int finalI = i;
             executorService.execute(()-> System.out.println(Thread.currentThread().getName()+",正在执行:"+ finalI));
         }
-
-
     }
 
 }
